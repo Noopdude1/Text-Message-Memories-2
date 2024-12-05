38317915386a7bb39517cbcbd8124b3cf4e38dfb
asfandsaddiqui <asfandsaddiqui@gmail.com>
2024-12-05 09:49:47 +0500

    replace npm with yarn

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Constants
-const PORT = process.env.PORT || 5000;
+const PORT =  process.env.PORT || 4000;
 const CURRENCY = { USD: 'usd' };
 
 // Utility: Error handler middleware
